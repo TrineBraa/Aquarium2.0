03a1f6f721ce0b307125d70324b716af96d384a6
trinebra <157688079+trinebra@users.noreply.github.com>
2024-08-27 14:58:05 +0200

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using Aquarium2._0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium2._0
+{
+    internal class Main
+    {   
+        
+        public void main()
+        {
+
+        }
+
+
+
+
+
+    }
+}
